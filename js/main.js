febd9a4fbc10dfecc4b89e0d41641087076bc258
thisisjsimon <jonahsimon06@gmail.com>
2024-08-24 11:42:42 +0200

    Started with a drop down menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,22 @@ button.addEventListener('click', () => {
     button.textContent = 'Abitur';
   }
 });
+
+
+
+const submenus = document.querySelectorAll('#menu ul');
+
+submenus.forEach(submenu => {
+  let timeout;
+
+  submenu.addEventListener('mouseenter', () => {
+    submenu.style.opacity = 1;
+    clearTimeout(timeout); // Check if timeout exists before clearing
+  });
+
+  submenu.addEventListener('mouseleave', () => {
+    timeout = setTimeout(() => {
+      submenu.style.opacity = 0;
+    }, 500); // Adjusted delay to 500ms
+  });
+});
