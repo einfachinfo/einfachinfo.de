8e29870e285ad8952cb86eff2da3899a4d40804f
LemonElias <dev.eliasweiss@gmail.com>
2024-09-12 13:47:50 +0200

    Started with "Von-Neumann-Architektur" and added Tooltips + Infofield for "Systembus"

---
+++ js/von_neumann_architektur.js
@@ -0,0 +1,19 @@
+function openPopup() {
+    const over =
+        document.getElementById(
+            "overlay"
+        );
+    const popDialog =
+        document.getElementById(
+            "popupDialog"
+        );
+    over.classList.toggle("hidden");
+    popDialog.classList.toggle(
+        "hidden"
+    );
+    popDialog.style.opacity =
+        popDialog.style.opacity ===
+            "1"
+            ? "0"
+            : "1";
+}
